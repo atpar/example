a05412922c0a6403fa546c6594ebf358b5044915
jo-es <escherich.johannes+github@gmail.com>
2020-11-27 13:18:20 +0100

    Fix example

--- create.ts
+++ create.ts
@@ -5,90 +5,82 @@ import ADDRESS_BOOK from '@atpar/protocol/ap-chain/addresses.json';
 import SettlementTokenArtifact from '@atpar/protocol/build/contracts/contracts/SettlementToken.sol/SettlementToken.json';
 
 import { keys, rpcURL } from './secret.json';
-import PAMTerms from './utils/PAMTerms.json';
+import PAMTerms from './PAMTerms.json';
 
-
-const deploySettlementToken =  async (web3: Web3, account: string) => {
-    // @ts-ignore
-    let sampleToken = new web3.eth.Contract(SettlementTokenArtifact.abi);
-    sampleToken = await sampleToken.deploy({ data: SettlementTokenArtifact.bytecode }).send({ from: account, gas: 2000000 });
-  
-    return sampleToken;
-}
 
 (async () => {
     const web3 = new Web3(new Web3.providers.HttpProvider(rpcURL));
 
+    // setup accounts
     keys.forEach((pk: string) => web3.eth.accounts.wallet.add(web3.eth.accounts.privateKeyToAccount(pk)));
 
     // set creator and counterparty accounts
-    const creator = (web3.eth.accounts.wallet[0]).address
-    const counterparty = (web3.eth.accounts.wallet[1]).address 
+    const creator = (web3.eth.accounts.wallet[0]).address; // lender with respect to the Terms
+    const counterparty = (web3.eth.accounts.wallet[1]).address; // debtor with respect to the Terms
     const anyone = (web3.eth.accounts.wallet[2]).address // used to make calls that could be made by any address
 
     // Initialize AP with web3 object and addressBook
-    const addressBook = ADDRESS_BOOK;
-    const ap = await AP.init(web3, addressBook);
+    const ap = await AP.init(web3, ADDRESS_BOOK);
 
     // Deploy Settlement Token
-    const settlementToken = await deploySettlementToken(web3, creator);
+    // @ts-ignore
+    const settlementToken = await (new web3.eth.Contract(SettlementTokenArtifact.abi)).deploy(
+        { data: SettlementTokenArtifact.bytecode }
+    ).send({ from: anyone, gas: 2000000 });
 
-    // get default PAM Terms and use the settlement token as the currency
-    const terms = {
-        ...PAMTerms,
-        currency: settlementToken.options.address
-    }
+    // create the term sheet by setting the currency to the Settlement Token contract we just deployed
+    const terms = { ...PAMTerms, currency: settlementToken.options.address };
     
     // set up ownership
     const ownership = {
-        creatorObligor: creator,
-        creatorBeneficiary: creator,
-        counterpartyObligor: counterparty,
-        counterpartyBeneficiary: counterparty,
-    }
+        creatorObligor: creator, // account which has to fulfill the lenders obligations (such as the initial exchange)
+        creatorBeneficiary: creator, // account which receives positive cashflows for the lender (such as interest payments)
+        counterpartyObligor: counterparty, // account which has to fulfill the debtors obligations (such as paying interest)
+        counterpartyBeneficiary: counterparty, //account which receives positive cashflows for the debtor (such as the principal)
+    };
 
-    // compute schedule from the terms using the PAM engine
-    const schedule = await ap.utils.schedule.computeScheduleFromTerms(ap.contracts.pamEngine, terms);
-    console.log(schedule)
-
-    // Create new PAM asset
+    // create new PAM asset
     const initializeAssetTx = await ap.contracts.pamActor.methods.initialize(
         terms, 
-        schedule, 
+        [], // optionally pass custom schedule, 
         ownership, 
         ap.contracts.pamEngine.options.address, 
         ap.utils.constants.ZERO_ADDRESS 
-    ).send({from: creator, gas: 2000000})
+    ).send({from: creator, gas: 2000000});
 
-    // Get AssetID
+    // retrieve the AssetId from the transaction event logs
     const assetId = initializeAssetTx.events.InitializedAsset.returnValues.assetId;
-    console.log('Asset created with ID: ' + assetId);
+    console.log('AssetId: ' + assetId);
 
+    // get asset terms
+    console.log('Terms: ', ap.utils.conversion.web3ResponseToState(
+        await ap.contracts.pamRegistry.methods.getTerms(assetId).call())
+    );
 
-    // Get asset terms
-    const assetTerms = await ap.contracts.pamRegistry.methods.getTerms(assetId).call();
-    const readableTerms = ap.utils.conversion.web3ResponseToPAMTerms(assetTerms);
-    console.log('Asset terms: ', readableTerms);
+    // get asset state
+    console.log('State: ', ap.utils.conversion.web3ResponseToState(
+        await ap.contracts.pamRegistry.methods.getState(assetId).call())
+    );
 
-    // Get asset state
-    const assetState = await ap.contracts.pamRegistry.methods.getState(assetId).call();
-    const readableState = ap.utils.conversion.web3ResponseToState(assetState);
-    console.log('Asset state: ', readableState);
-
-    // Get next scheduled event for the asset from the PAM Registry and decode it
+    // get next scheduled event for the asset from the PAM Registry and decode it
     const nextScheduledEvent = await ap.contracts.pamRegistry.methods.getNextScheduledEvent(assetId).call();
     const decodedEvent = ap.utils.schedule.decodeEvent(nextScheduledEvent);
-    console.log('Next scheduled event: ', decodedEvent)
+    // in our case that the IED (Initial Exchange event for transferring the principal from the lender to the creditor)
+    console.log('Next scheduled event: ', decodedEvent);
 
     // approve actor to execute settlement payment (must be called before progressing the asset)
-    const amount = readableTerms.notionalPrincipal
-    const currency = readableTerms.currency
-    await ap.contracts.erc20(currency).methods.approve(ap.contracts.pamActor.options.address, amount)
+    // lender has to give allowance to the Actor to transfer the principal to the debtor
+    await ap.contracts.erc20(terms.currency).methods.approve(
+        ap.contracts.pamActor.options.address,
+        terms.notionalPrincipal
+    ).send({from: creator, gas: 2000000 });
     
     // progress the asset - can be called by any account
+    // processes the first event (IED)
     const progressTx = await ap.contracts.pamActor.methods.progress(assetId).send({from: anyone, gas: 2000000});
-    console.log('Progress events: ', progressTx.events)
+    console.log('Progressed Event: ', progressTx.events.ProgressedAsset.returnValues);
 
-    console.log('\nDONE.')
-    process.exit()
+    // continue with getNextScheduledEvent for all subsequent events
+
+    process.exit();
 })();
